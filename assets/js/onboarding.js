7ef2848f461e948c42230142ca6f9610675570a9
Christiano112 <94331754+Christiano112@users.noreply.github.com>
2022-07-13 12:34:48 +0100

    Added the automatic slideshow

--- assets/js/onboarding.js
+++ assets/js/onboarding.js
@@ -1,3 +1,4 @@
+// SLIDESHOW
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -32,13 +33,31 @@ function showSlides(n) {
 
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
+}
 
-    // HIDING THE START AD NEXT BUTTONS
-    // if (slideIndex == 2) {
-    //     document.getElementsByClassName("dots2").className += " active";
-    // }
 
-    // if (slideIndex == 3) {
-    //     document.getElementsByClassName("dots3").className += " active";
-    // }
+// AUTOMATIC SLIDESHOW
+
+let slideIndexAuto = 0;
+
+function showSlidesAuto() {
+    let i;
+    let slides = document.getElementsByClassName("slides");
+    let dots = document.getElementsByClassName("dots");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndexAuto++;
+    if (slideIndexAuto > slides.length) { slideIndexAuto = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndexAuto - 1].style.display = "block";
+    dots[slideIndexAuto - 1].className += " active";
+    setTimeout(showSlidesAuto, 5000);
+}
+
+// Call Function on Tablet Screen and Mobile Screen
+if (window.matchMedia("(max-width: 1024px)").matches) {
+    showSlidesAuto();
 }
